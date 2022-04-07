e08be643892b9f28872a83f3f2d6c63ea038c410
edmcorrea <edm.correa@hotmail.com>
2022-04-07 15:37:51 -0300

    exercicios do dia finalizados

---
+++ fundamentos/bloco-04-introducao-a-js-e-logica/dia-2-array-e-loops/script.js
@@ -0,0 +1,74 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//exercicio 1
+// for(i=0; i<numbers.length; i++){
+//   console.log(numbers[i]);
+// }
+
+//exercicio 2
+// let soma=0;
+// for(i=0; i < numbers.length; i++){
+//   soma += numbers[i];
+// }
+// console.log(soma);
+
+//exercicio 3 e 4
+// let soma=0;
+// for(i=0; i < numbers.length; i++){
+//   soma = soma + numbers[i];  
+// }
+
+// let media = soma/numbers.length;
+// if(media>20){
+//   console.log("Media maior que 20");
+// }else{
+//   console.log("Media menor que 20");
+// }
+
+//exercicio 5
+// let aux=numbers[0];
+// for(i=0; i<numbers.length; i++){
+//   if(numbers[i]>aux){
+//     aux=numbers[i];
+//   }
+// }
+// console.log(aux);
+
+//exercicio 6 
+// let impar = 0;
+// let par = 0;
+// for(i=0; i<numbers.length; i++){
+//   let aux = numbers[i]%2
+//   if(aux==1){
+//     impar+=1;
+//   }else{
+//     par+=1;
+//   }
+// }
+// if(impar==0){
+//   console.log("nenhum valor ímpar encontrado");
+// }else{
+//   console.log(impar);
+// }
+
+//exercicio 7
+// let aux=numbers[0];
+// for(i=0; i<numbers.length; i++){
+//   if(numbers[i]<aux){
+//     aux=numbers[i];
+//   }
+// }
+// console.log(aux);
+
+//exercicio 8 e 9
+let a=[];
+let divisor=[];
+for(i=1; i<=25; i++){
+  a.push(i);  
+  divisor.push(a[i-1]%2);
+}
+console.log(a);
+console.log(divisor);
+
+let num=2%2;
+console.log(num);
