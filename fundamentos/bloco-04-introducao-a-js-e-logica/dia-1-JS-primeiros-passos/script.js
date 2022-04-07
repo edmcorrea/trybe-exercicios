43a9e8f4a8dc038154fec84a6dd6f5c09465cbf7
edmcorrea <edm.correa@hotmail.com>
2022-04-07 06:33:57 -0300

    exercicio pela metade

--- fundamentos/bloco-04-introducao-a-js-e-logica/dia-1-JS-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica/dia-1-JS-primeiros-passos/script.js
@@ -1,5 +1,42 @@
-const myName = "Edmilson";
-const birthCity = "Feira de Santana";
-let birthYear = 1994;
+/*
+console.log("Atividade 1"); 
+let a = 3;
+let b = 5;
+let c = 7;
 
-console.log(myName); 
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+    console.log(a%b);
+
+console.log("Atividade 2"); 
+
+if(a>b){
+    console.log("o maior numero é "+a);
+}else{
+    console.log("o maior numero é "+b);
+}
+
+console.log("Atividade 3"); 
+if(a>b && a>c){    
+    console.log("o maior numero é "+a);
+    
+}else if (b>a && b>c){    
+    console.log("o maior numero é "+b);
+    
+}else{
+    console.log("o maior numero é "+c);
+}*/
+
+console.log("Atividade 4");
+const valor=-4;
+if(valor>0){    
+    console.log("positive");
+    
+}else if (valor==0){    
+    console.log("zero");
+    
+}else{
+    console.log("negative");
+}
