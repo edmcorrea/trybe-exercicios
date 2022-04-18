78fbad7ce2f65251be1d2003cf5a2eb35f785811
edmcorrea <edm.correa@hotmail.com>
2022-04-18 11:30:42 -0300

    adicionando atividades anteriores

---
+++ fundamentos/bloco-05-js-dom-eventos/dia-02-js-trabalhando-com-elementos/pre-aula/script.js
@@ -0,0 +1,11 @@
+let container = document.querySelector("#pai");
+let aside = document.createElement("aside");
+container.appendChild(aside);
+
+let primeirofilhodofilho = document.getElementById('primeiroFilhoDoFilho');
+let filho = document.createElement('aside');
+primeirofilhodofilho.appendChild(filho);
+
+let query = document.querySelector("#pai");
+let filho1 = document.createElement('aside');
+query.appendChild(filho1);
--- fundamentos/bloco-05-js-dom-eventos/dia-02-js-trabalhando-com-elementos/script.js
+++
@@ -1,1 +0,0 @@
-document.querySelector("#elementoOndeVoceEsta").parentNode;
