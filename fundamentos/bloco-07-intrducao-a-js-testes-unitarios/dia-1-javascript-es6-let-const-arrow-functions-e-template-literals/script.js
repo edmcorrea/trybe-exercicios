f46753a64b068e29e0a2ff6e3cf304a19c33135f
edmcorrea <edm.correa@hotmail.com>
2022-05-07 15:27:58 -0300

    exercicios do dia até o 4 1/2

--- fundamentos/bloco-07-intrducao-a-js-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-07-intrducao-a-js-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,11 +1,13 @@
+//PARTE 1
+
 // 1. 🚀 Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
 // Modifique a estrutura da função para que ela seja uma arrow function.
 // Modifique as concatenações para template literals.
 // Copie o código abaixo.
 const testingScope = (escopo) => {
-  if (escopo === true) {
+  if (escopo) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    ifScope += ' ótimo, fui utilizada no escopo !';
     console.log(ifScope);
   } else {
     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
@@ -13,7 +15,7 @@ const testingScope = (escopo) => {
   };  
 }
 
-testingScope(true);
+// testingScope(true);
 
 // 2. Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
 // Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
@@ -27,4 +29,82 @@ const sortsFunction = (a, b) => a-b;
 
 oddsAndEvens.sort(sortsFunction);
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+// console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+
+
+//PARTE 2
+
+// 1. Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+
+const calculoFatorial = param => {
+  let fatorial = 1;  
+  for (let i = 1; i <= param; i+=1) {
+    fatorial *= i;
+  }
+  return fatorial;
+}
+console.log(calculoFatorial(4));
+
+// 2. Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const longestWord = (phrase) => {
+  const array = phrase.split(' ');
+  let biggestPhrase = array[0];
+  for (let i in array) {
+    if (biggestPhrase.length < array[i].length) {
+      biggestPhrase = array[i];
+    }
+    // biggestPhrase = (biggestPhrase.length < array[i].length) ?  : array[i];
+  }
+  return biggestPhrase;
+}
+
+// console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+
+// 🚀 Crie uma página que contenha:
+// Um botão ao qual será associado um event listener;
+// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+
+const eventoClique = () => {
+  const button = document.querySelector(".button");
+  const p = document.querySelector(".count");
+  let clickCount = 0;
+  p.innerHTML = `Você clicou ${clickCount} VEZES no botão "Clique Aqui`;  
+  button.addEventListener("click", () => p.innerHTML = `Você clicou ${clickCount+=1} VEZES no botão "Clique Aqui`);  
+}
+
+// eventoClique();
+
+// 4. 🚀 Crie um código JavaScript com a seguinte especificação:
+// Não se esqueça de usar template literals
+// Função 1: Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string.
+// Exemplo:
+// String determinada: "Tryber x aqui!"
+// Parâmetro: "Bebeto"
+// Retorno: "Tryber Bebeto aqui!"
+
+const skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+
+const substituiX = (string) => {
+  const substituto = 'edmix';
+  let novaString = "";
+  for (let i = 0; i < string.length; i+=1) {
+    (string[i]=== 'x') ? novaString += skills[Math.floor(Math.random()*5)] : novaString += string[i];
+    console.log(novaString);
+  }
+  return novaString;
+}
+console.log(substituiX("Tryber x aqui!"));
+
+// Um array com escopo global, que é o escopo do arquivo JS, nesse caso, contendo cinco strings com suas principais skills.
+// Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string.
+// Exemplo:
+// "Tryber x aqui!
+// Minhas cinco principais habilidades são:
+// JavaScript;
+// HTML; ...
+// #goTrybe".
