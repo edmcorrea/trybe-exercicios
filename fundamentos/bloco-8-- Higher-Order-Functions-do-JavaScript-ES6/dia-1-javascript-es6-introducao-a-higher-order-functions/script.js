d5b64ab77388fb2a90e6a57b74859f9a7f0f5a3f
edmcorrea <edm.correa@hotmail.com>
2022-05-13 13:22:08 -0300

    adicionando script pre aula

---
+++ fundamentos/bloco-8-- Higher-Order-Functions-do-JavaScript-ES6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,26 @@
+
+// ################## NÃO ENTENDI ###############
+const sumFixAmount = (amount) => {
+  return (number) => amount + number;
+}
+const initialSum = sumFixAmount(15)
+console.log(initialSum(5));
+
+//EXEMPLO 2
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+repeat(5, (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+});
+
+const numberGenerator = () => {
+  return Math.random() * 100;
+}
+
+console.log(numberGenerator);
