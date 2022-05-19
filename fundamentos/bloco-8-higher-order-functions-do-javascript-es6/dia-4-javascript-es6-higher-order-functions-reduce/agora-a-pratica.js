ae99361dc2931946b593f97e713cda2b845e8dad
edmcorrea <edm.correa@hotmail.com>
2022-05-18 22:31:49 -0300

    correcoes do elemento adicionado

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/agora-a-pratica.js
@@ -0,0 +1,168 @@
+// 1 - Dada uma matriz, transforme em um array.
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, element) => acc.concat(element, []));
+}
+
+// console.log(flatten());
+
+// #####################################################
+// Para os exercícios 2, 3 e 4 considere o seguinte array:
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+
+//2 - Crie uma string com os nomes de todas as pessoas autoras.
+// const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames() {
+  return books.reduce((acc, element) => {
+    return `${acc} ${element.author.name},`;
+  }, "")
+}
+// console.log(reduceNames());  //REVER CODIGO
+
+// 🚀 3- Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+// const expectedResult = 43;
+
+function averageAge() {
+  return books.reduce((acc, element, index) => {
+    return acc + element.releaseYear-element.author.birthYear;
+  }, 0)/(books.length);
+}
+
+// console.log(averageAge());
+
+
+// 🚀 4- Encontre o livro com o maior nome.
+// const expectedResult = {
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   genre: 'Fantasia',
+//   author: {
+//     name: 'George R. R. Martin',
+//     birthYear: 1948,
+//   },
+//   releaseYear: 1991,
+// };
+
+function longestNamedBook() {
+  return books.reduce((acc, element) => (acc.name.length > element.name.length)? acc : element);
+}
+
+// console.log(longestNamedBook());
+
+
+
+
+// 🚀 5- Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra 'a' maiúscula ou minúscula.
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA(names) {
+  return names.reduce((acc, element) => {     
+    return acc + element.split('').reduce((acc1, element2) => {
+      (element2 === 'a' || element2 === 'A') ? acc1 +=1 : null; 
+      return acc1;
+    }, 0);
+    
+    // return element.forEach((element, index) => {
+      // return (element[index] === 'a') ? acc += 1 : null;
+    
+  }, 0)
+}
+console.log(containsA(names));
+
+// 🚀 6.**- Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos, aqui além de reduce será necessário utilizar também a função map. Dica: Você pode acessar o index do array dentro de map, e você pode ver o objeto esperado na constante expected.
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage(students, grades) {
+  return students.map((element, index) => {
+    return {
+      name: element,
+      average: grades[index].reduce((acc, element2) => (
+        acc + element2)
+      )/grades[index].length
+    }
+  })
+}
+
+console.log(studentAverage(students, grades));
+
+
