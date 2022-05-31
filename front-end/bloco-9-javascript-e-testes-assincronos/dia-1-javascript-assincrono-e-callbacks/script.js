b3cb6cefa140305cfa31baf7877614a1b20d37fd
edmcorrea <edm.correa@hotmail.com>
2022-05-31 20:02:22 -0300

    atividade do dia ate questao 6

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -0,0 +1,102 @@
+// const despesas = [
+//   {
+//     gym: 99,
+//   },
+//   {
+//     ifood: 200,
+//   },
+//   {
+//     phone: 60,
+//   },
+//   {
+//     internet: 100,
+//   },
+// ];
+
+// const renda = 1000;
+
+// const despesaMensal = (renda, despesas, callback) => {
+
+//   const despesaTotal = callback(despesas);
+//   const saldoFinal = renda - despesaTotal;
+
+//   console.log(`Balanço do mês:
+//     Recebido: R$${renda},00
+//     Gasto: R$${despesaTotal},00
+//     Saldo: R$${saldoFinal},00 `);
+// };
+
+// const despesasTotal = (despesas) => {
+//   return despesas.map((element) => {
+//     return Object.values(element);
+//   })
+// }
+
+// console.log(despesasTotal(despesas));
+
+
+
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// const getUser = () => {
+//   const userToReturn = {
+//     firstName: "Ivan",
+//     lastName: "Ivanovich",
+//     nationality: "Russian"
+//   };
+//   // Insira o retorno da função `getUser`
+// };
+
+// console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+// console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// const getUser = (callback) => {
+//   const userToReturn = {
+//     firstName: "Ivan",
+//     lastName: "Ivanovich",
+//     nationality: "Russian",
+//   };
+
+//   return callback(userToReturn);
+// };
+
+// console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+// console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+
+
+
+// ######################### LIDANDO COM ERROS EM OPERAÇÕES ASSINCRONAS ####################################
+
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 1000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onFail) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    } else {
+      const errorMessage = "Country could not be found";
+      onFail(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryCurrency, printErrorMessage);
