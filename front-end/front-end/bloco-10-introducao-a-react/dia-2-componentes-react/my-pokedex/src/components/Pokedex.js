721cbc0e64f4e209f78cad503fa8b84d5912095c
edmcorrea <edm.correa@hotmail.com>
2022-06-21 14:31:17 -0300

    inicio dos quesitos de solar system

--- front-end/front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.js
+++ front-end/front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.js
@@ -1,24 +1,24 @@
 import React from "react";
-import Pokemon from './Pokemon';
-import data from '../data';
+import Pokemon from "./Pokemon";
+import data from "../data";
 import Header from "./Header";
 
 class Pokedex extends React.Component {
   render() {
     return (
       <div>
-        <Header titulo={Pokedex}/>
-          {data.map((element) => (
-            <Pokemon 
-              key={element.id}
-              name={element.name} 
-              type={element.type} 
-              averageWeight={element.averageWeight} 
-              image={element.image}
-            />
-          ))}
+        <Header titulo={Pokedex} />
+        {data.map((element) => (
+          <Pokemon
+            key={element.id}
+            name={element.name}
+            type={element.type}
+            averageWeight={element.averageWeight}
+            image={element.image}
+          />
+        ))}
       </div>
-    )
+    );
   }
 }
 
